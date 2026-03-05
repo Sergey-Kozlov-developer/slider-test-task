14987e82c262e97f9525792d03acfa62084ed32f
Sergei Kozlov <sergeikozlov@MacBook-Air-Sergei.local>
2026-03-05 14:18:42 +0300

    init app & add fsd  & add webpack, ts, babel configs

---
+++ README.md
@@ -0,0 +1,34 @@
+# Slider Task
+
+## Запуск проекта
+
+1. **Клонировать репозиторий**
+
+   ```bash
+   git clone https://github.com/Sergey-Kozlov-developer/slider-test-task.git
+   ```
+
+2. **Установить зависимости**
+
+   ```bash
+   npm install
+   ```
+
+3. **Запустить проект в режиме разработки**  
+   (проект собирается с помощью Webpack)
+
+   ```bash
+   npm run dev
+   ```
+
+4. **Открыть в браузере**  
+   Перейдите по адресу: [http://localhost:3000](http://localhost:3000)
+
+## Стек технологий
+
+- **React**
+- **TypeScript**
+- **GSAP**
+- **SCSS**
+- **Webpack**
+- **FSD (Feature-Sliced Design)**
---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {
+	CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+
+module.exports = {
+	entry: "./src/index.tsx",
+	devtool: "inline-source-map",
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+	},
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"],
+		alias: {
+			"@app": path.resolve(__dirname, "./src/app"),
+			"@shared": path.resolve(__dirname, "./src/shared"),
+			"@entities": path.resolve(__dirname, "./src/entities"),
+			"@features": path.resolve(__dirname, "./src/features"),
+			"@widgets": path.resolve(__dirname, "./src/widgets"),
+			"@pages": path.resolve(__dirname, "./src/pages"),
+			"@styles": path.resolve(__dirname, "./src/shared/styles"),
+			"@assets": path.resolve(__dirname, "./src/shared/assets"),
+			"@static": path.resolve(__dirname, "./src/shared/static"),
+			"@ui": path.resolve(__dirname, "./src/shared/ui"),
+			"@lib": path.resolve(__dirname, "./src/shared/lib")
+		},
+	},
+	module: {
+		rules: [{
+				test: /\.(ts|tsx)$/,
+				exclude: /node_modules/,
+				use: "babel-loader",
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					// { loader: "style-loader" },
+					{
+						loader: "css-loader"
+					},
+					{
+						loader: "sass-loader"
+					},
+				],
+			},
+			{
+				test: /\.css?$/,
+				use: [MiniCssExtractPlugin.loader, {
+					loader: "css-loader"
+				}],
+			},
+			{
+				test: /\.svg$/i,
+				issuer: /\.[jt]sx?$/,
+				use: ["@svgr/webpack"],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./public/index.html",
+		}),
+		new MiniCssExtractPlugin({
+			filename: "main.bundle.css",
+		}),
+		new CleanWebpackPlugin(),
+	],
+	devServer: {
+		static: {
+			directory: path.join(__dirname, "dist"),
+		},
+		compress: true,
+		port: 3000,
+	},
+};
